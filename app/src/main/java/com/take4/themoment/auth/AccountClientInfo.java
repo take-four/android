b967b17d86f17166383aecb9cdc78e8b1d213b5f
Park-JaeHyun <flagbug12@gmail.com>
2018-01-29 01:38:47 +0900

    Remove non use class file

--- app/src/main/java/com/take4/themoment/auth/AccountClientInfo.java
+++
@@ -1,9 +0,0 @@
-package com.take4.themoment.auth;
-
-/**
- * Created by jaehyunpark on 2018. 1. 6..
- */
-
-public interface AccountClientInfo {
-	String GOOGLE_SERVER_CLIENT_ID = "291430050015-ebi4ahua78f7aj2m26lo4rsivjocgaqr.apps.googleusercontent.com";
-}
